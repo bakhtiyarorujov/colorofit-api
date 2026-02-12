c0933bbabb553dfc8ec0fc09ce83df637e77db89
sabinaorujova <sabinaorujva@gmail.com>
2026-02-12 15:11:30 +0400

    bugfix permisson

--- food/views.py
+++ food/views.py
@@ -754,6 +754,7 @@ class WaterIntakeDailyTotalView(APIView):
     }
 )
 class DailyStatsView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         # Filter for today's food items for the logged-in user
         today = timezone.now().date()
